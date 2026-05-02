8d2ab0282b42452a29d97db56c7c633994c6f41d
imsresearch <emsdilemma@proton.me>
2026-05-02 16:24:02 +0100

    Create website files

---
+++ precache-manifest.d9debfe101a811b4c5e2b36e297cea5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de9874aeef7fa7a1e3dd79ef55c04dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b173c41116eec2d703",
+    "url": "/static/js/2.41b2878e.chunk.js"
+  },
+  {
+    "revision": "acaccd3a11c5c4db30b41f14d8e96ab6",
+    "url": "/static/js/2.41b2878e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0855e069e58ae5cd6739",
+    "url": "/static/js/main.9c23508c.chunk.js"
+  },
+  {
+    "revision": "1d9e8987070f765e9940",
+    "url": "/static/js/runtime-main.9f0f4fa3.js"
+  }
+]);
