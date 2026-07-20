343c9cdc54b6ab20ebcc22a029448cd028528bcb
imsresearch <emsdilemma@proton.me>
2026-07-20 16:54:52 +0800

    Wrong site message only when at wrong site

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(
-    document.getElementById("root")
-).render(
-    <App />
-);
+if (window.location.href == "https://imsresearch.github.io/") {
+    document.getElementById("root").innerHTML = '<h1>The IMS website is now at <a href="https://imsresearch.pages.dev/">https://imsresearch.pages.dev</a>!</h1>';
+}
+else {
+    document.getElementById("root").innerHTML = "<p>not at github</p>";
+    console.log("not github");
+
+    ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+}
