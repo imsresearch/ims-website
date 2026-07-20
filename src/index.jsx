5d568db8533b991a1243da283f2dd6d9873f12cd
imsresearch <emsdilemma@proton.me>
2026-07-20 16:57:19 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 if (window.location.href == "https://imsresearch.github.io/") {
-    document.getElementById("root").innerHTML = '<h1>The IMS website is now at <a href="https://imsresearch.pages.dev/">https://imsresearch.pages.dev</a>!</h1>';
+    document.getElementById("root").innerHTML = '<div style="text-align: centre;"><h1>The IMS website is now at <a href="https://imsresearch.pages.dev/">https://imsresearch.pages.dev</a>!</h1></div>';
+    console.log("at github.io, showing warn")
 }
 else {
-    document.getElementById("root").innerHTML = "<p>not at github</p>";
-    console.log("not github");
+    console.log("not at github, running app");
 
     ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 }
