ce517de63cf51af0a9242ca61fdc145fb0aa9946
imsresearch <emsdilemma@proton.me>
2026-07-27 01:07:57 +0800

    Update <head> and 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 
+import TopHeader from "../components/TopHeader";
+import LowerHeader from "../components/LowerHeader";
+import Footer from "../components/Footer";
+
+import "../styles/404.css";
+
 export default function NotFound() {
     return (
-        <div
-            style={{
-                minHeight: "100vh",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                background: "#111",
-                color: "white",
-            }}
-        >
-            <h1>Page not found! :d</h1>
-            <a href="/">Return to home</a>
-            <p>:3 wwwrrrruffff</p>
+        <div className="404-page">
+            <TopHeader />
+            <LowerHeader />
+
+            {/* journal viewer */}
+            <main className="journal-section">
+                <div className="content">
+                    <h1>Page not found</h1>
+                    <p>The page you requested may be broken, moved, or no longer exist.</p>
+                    <p>If you believe this is a mistake, contact us on <a href="https://discord.gg/vHnMhwdZA3">Discord</a> (vHnMhwdZA3)</p>
+                </div>
+            </main>
+
+            <Footer />
         </div>
     );
 }
