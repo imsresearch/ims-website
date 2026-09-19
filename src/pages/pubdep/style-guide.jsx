2e40ee11965b558be23c958821570ceeb81550b9
imsresearch <emsdilemma@proton.me>
2026-09-19 23:35:19 +0100

    Update style guide

--- src/pages/pubdep/style-guide.jsx
+++ src/pages/pubdep/style-guide.jsx
@@ -21,12 +21,12 @@ export default function StyleGuide() {
             <LowerHeader />
 
             <section className="top-section">
-                {/* sidebar */}
+                {/*sidebar*/}
                 <aside className="style-sidebar" id="sidebar">
                     <a href="#layout">Layout</a>
                     <a href="#abstract">Abstract</a>
                     <a href="#introduction">Introduction</a>
-                    <a href="#body">Main text and style throughout the manuscript</a>
+                    <a href="#body">Body</a>
                     <a href="#diagrams">Diagrams (optional)</a>
                     <a href="#conclusion">Conclusion</a>
                     <a href="#references">References and acknowledgements</a>
@@ -48,7 +48,10 @@ export default function StyleGuide() {
                         <br />
                         <br />
                         <small>
-                            Last updated <time dateTime="2026-08-30T20:26" title="2026-08-30T21:26">2026-08-30 20:26 UTC</time>
+                            Last updated{" "}
+                            <time dateTime="2026-09-19T22:32" title="2026-09-19T22:32">
+                                2026-09-19 22:32 UTC
+                            </time>
                         </small>
                     </p>
                 </div>
@@ -64,7 +67,7 @@ export default function StyleGuide() {
                     <Tooltip id="layout-tooltip" />
                     <ul>
                         <li>The below structure should be used for journal articles: (ignoring irrelevant items)</li>
-                        <li><a href="https://storage.googleapis.com/plos-corpus-prod/10.1371/journal.pcbi.1005830/1/pcbi.1005830.g001.PNG_L?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=wombat-sa%40plos-prod.iam.gserviceaccount.com%2F20260821%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20260821T204351Z&X-Goog-Expires=86400&X-Goog-SignedHeaders=host&X-Goog-Signature=42898b30cf9747f7cea0d13a847fa094496332b0e5addb910dfb59a74d68d05bbddf1c48dfa627b2f8372cef85e01f3ba32ba2adfbef867f86e3065b8c5c5f8a5550a170e538580a9054071a2457a4abf5ffa8eef7cf65a4438b12b603152a29333b722d368427a4fe33edc66933ce692d107ee5c69ddd6688ec3afdccfd099c4172fb98196eaa405def4fa6ca6438b9fb1a0efcc2da8a6ca24f6cea5cd1425d3f277457cd94f0073fe0641d2db9a3438090aa8ce8a9f0a175077c1dd1a06987168beefbf074b8a8c5590bf7e7a0e8eb3648f0144e1f47c364d253360533eb54f8950199dd033f63dbf44a1f4ac11b6671ce988e1e956186eef189c76138ac43">This image</a> explains the role of each section</li>
+                        <li><a href="https://journals.plos.org/ploscompbiol/article/figure/image?download&size=large&id=10.1371/journal.pcbi.1005830.g001">This image</a> explains the role of each section</li>
                     </ul>
                     <p>
                         Title
@@ -122,33 +125,72 @@ export default function StyleGuide() {
 
                     <h2 id="body">
                         <span data-tooltip-id="body-tooltip" data-tooltip-content="#body">
-                            Main text <a href="#sidebar">(top)</a>
+                            Body <a href="#sidebar">(top)</a>
                         </span>
                     </h2>
                     <Tooltip id="body-tooltip" />
                     <ul>
-                        <li>Text must be well written, consistent, clear, and have good grammar</li>
-                        <li>Text must be succinct. Sentences shouldn't meander on or use too many words to say something simple. Authors should avoid redundant words</li>
-                        <li>Authors may use American, Commonwealth, or any other formal spelling conventions</li>
-                        <li>More formal articles should avoid puffery (e.g. best, greatest, groundbreaking), weasel words (e.g. some people say, experts state, science shows, is widely regarded), editorializing (e.g. interestingly, actually, clearly, of course, unfortunately), colloquial language, and vague phrasing</li>
-                        <li>
+                        <li>Conventions</li>
+                        <ul>
+                            <li>Any date format may be used, but they should be used consistently.</li>
+                            <li>Authors may use American, Commonwealth, or any other formal spelling conventions</li>
+                            <li>Always spell out the names for months.</li>
+                        </ul>
+
+                        <li>Tone and manner of writing</li>
+                        <ul>
+                            <li>More formal articles should avoid puffery (e.g. best, greatest, groundbreaking), weasel words (e.g. some people say, experts state, science shows, is widely regarded), editorializing (e.g. interestingly, actually, clearly, of course, unfortunately), colloquial language, and vague phrasing</li>
+                            <li>Text must be well written, consistent, clear, and have good grammar</li>
+                            <li>Text must be succinct. Sentences shouldn't meander on or use too many words to say something simple. Authors should avoid redundant words</li>
+                            <li>Authors should be attentive to a reader’s ease of reading, for example by taking care of excessively long run-on sentences</li>
+                        </ul>
+
+                        <li>Contractions</li>
+                        <ul>
+                            <li>Abbreviations and contractions (e.g. ASAP, N/A, can't, they're) should be avoided except for academic abbreviations such as e.g., i.e., and et al.</li>
+                            <li>
                             Acronyms must be defined upon first use, except if they are in the <a>official glossary</a>
                             <br />
                             <small>The glossary has not been set up yet — please ask in the Publishing Department server if you are unsure.</small>
-                        </li>
-                        <li>Newly introduced terms should (but do not need to) be italicised</li>
-                        <li>Authors should be attentive to a reader’s ease of reading, for example by taking care of excessively long run-on sentences</li>
-                        <li>
-                            Footnotes must be inserted using superscripted letters of the alphabet, with the footnote below the margins of the page
-                            <br />
-                            e.g. Lorem ipsum
-                            <sup>a</sup> dolor sit amet...
-                        </li>
-                        <li>Abbreviations for phrases (e.g. ASAP, N/A) should be avoided except for academic abbreviations such as e.g., i.e., and et al.</li>
-                        <li>Should include the key question posited by the article and the methods that it uses to answer the question</li>
-                        <li>The text must comply with copyright laws. Instances of fair use must be marked and justified</li>
-                        <li>All quotations must use <a href="https://en.wikipedia.org/wiki/Wikipedia:Manual_of_Style#Punctuation_inside_or_outside">logical quotation</a> for clarity and consistency</li>
-                        <li>Section titles should use sentence case</li>
+                            </li>
+                        </ul>
+
+                        <li>Quotations</li>
+                        <ul>
+                            <li>All quotations must use <a href="https://en.wikipedia.org/wiki/Wikipedia:Manual_of_Style#Punctuation_inside_or_outside">logical quotation</a> for clarity and consistency</li>
+                        </ul>
+                        
+                        <li>Formatting</li>
+                        <ul>
+                            <li>Section titles should use sentence case</li>
+                            <li>Newly introduced terms should (but do not need to) be italicised</li>
+                        </ul>                        
+                        
+                        <li>Citaitons</li>
+                            <ul>
+                                <li>
+                                Footnotes must be inserted using superscripted letters of the alphabet, with the footnote below the margins of the page
+                                <br />
+                                e.g. Lorem ipsum <sup>a</sup> dolor sit amet...
+                                </li>
+                                <li>
+                                    Numeric citations must be inserted using numbers corresponding to the citation's number in the bibliography in square brackets
+                                    <br />
+                                    e.g. Lorem ipsum [1] dolor sit amet...
+                                    <br />
+                                    [1] John Charles, 2026...
+                                </li>
+                            </ul>
+
+                        <li>Copyright</li>
+                        <ul>
+                            <li>The text must comply with copyright laws. Instances of fair use must be marked and justified</li>
+                        </ul>
+
+                        <li>Contents</li>
+                        <ul>
+                            <li>Should include the key question posited by the article and the methods that it uses to answer the question</li>
+                        </ul>
                     </ul>
                     <br />
 
